322b950274296561fc31918a3a45a7bef18dd00e
s-cavalier <jrc974@ucsb.edu>
2026-04-03 17:27:26 -0700

    bruh

--- parameter_sweep.py
+++ parameter_sweep.py
@@ -7,7 +7,7 @@ from tqdm import trange, tqdm
 import matplotlib.pyplot as plt
 from time import sleep
 
-from jaxcurve import CompiledControls, LearnableCurve, make_traceless_H, sig_z
+from jaxcurve import CompiledControls, LearnableCurve, make_traceless_H, sig_z, sig_x, sig_y
 from jaxcurveimpls import BezierCurve
 
 if __name__ == "__main__":
@@ -16,52 +16,55 @@ if __name__ == "__main__":
     runs = int(argv[2])
     use_callback = bool(argv[3])
 
-    errors = np.empty((runs, 3))
+    errors = np.empty((runs, 4))
 
     iters = trange(runs) if use_callback else range(runs)
 
     key = jax.random.key(seed)
-    curve = BezierCurve(32, [0, 0, 0], key)
-    system = CompiledControls(curve)
+    curve : BezierCurve = tree_deserialise_leaves('bezier.eqx', BezierCurve(32, [0, 0, 0], key))
+    coefs = jnp.array([-1/6, 0, 1/120, 0, -1/5040])
 
-    tree_deserialise_leaves('bezier.eqx', curve)
+    system = CompiledControls(curve)
 
     for sweep_idx in trange(runs):
         key, subkey = jax.random.split(key, 2)
 
-        paulis = jax.random.uniform(subkey, (2,), maxval=0.4999999)
-        
+        paulis = jax.random.uniform(subkey, (3,), maxval=0.3333)
+
         errors[sweep_idx, 0] = paulis[0].item()
         errors[sweep_idx, 1] = paulis[1].item()
+        errors[sweep_idx, 2] = paulis[2].item()
 
-        H_in = make_traceless_H(*paulis, 0)
+        H_in = make_traceless_H(*paulis)
 
         diffrax_res = system(H_in)
         realized_unitary = jnp.asarray( diffrax_res.ys )[0]
 
-        pow2 : jnp.ndarray = jnp.linalg.matrix_power(H_in, 2)
-        pow4 : jnp.ndarray = jnp.linalg.matrix_power(H_in, 4)
-        pow6 : jnp.ndarray = jnp.linalg.matrix_power(H_in, 6)
+        def step(carry, _):
+            carry = carry @ H_in
+            return carry, carry
+        
+        _, powers = jax.lax.scan(step, H_in, None, length=5)
 
-        target_polynomial = pow2 / 6 + pow4 / 120 - pow6 / 5040
-        expected = jax.scipy.linalg.expm(-1j * jnp.kron(target_polynomial, sig_z()))
+        expected = jax.scipy.linalg.expm(-1j * jnp.kron( jnp.sum(coefs[:, None, None] * powers, axis=0), sig_z() ) )
 
         final_error : jnp.ndarray = jnp.linalg.matrix_norm( realized_unitary - expected, ord=2 )
 
-        errors[sweep_idx, 2] = final_error.item()
+        errors[sweep_idx, 3] = final_error.item()
 
-    np.save('errors.npz', errors)
-
-    data = np.load
+    np.save('errors', errors)
 
     fig = plt.figure(figsize=(10, 10))
     ax = fig.add_subplot(projection='3d')
 
-    p = ax.scatter( errors[:, 0], errors[:, 1], errors[:, 2], c=errors[:, 3])
+    p = ax.scatter( errors[:, 0], errors[:, 1], errors[:, 2], c=errors[:, 3], cmap='viridis' )
 
     ax.set_xlabel(r'$\sigma_x$')
     ax.set_ylabel(r'$\sigma_y$')
     ax.set_zlabel(r'$\sigma_z$')
 
-    fig.savefig('error_sweep2d.png')
+    cb = fig.colorbar(p, ax=ax)
+    cb.set_label('Error')
+
+    fig.savefig('error_sweep.png')
 
